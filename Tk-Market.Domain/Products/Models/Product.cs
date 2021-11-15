65b7da470c411f4ecdb41d04615d015c924a278a
Maikon Takeshi Tsujiguchi <maikon_numb@hotmail.com>
2021-11-15 15:56:23 -0300

    Adicionado classe de produtos

---
+++ Tk-Market.Domain/Products/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace Tk_Market.Domain.Products.Models
+{
+    public class Product
+    {
+        public long Id { get; set; }
+        public long Code { get; set; }
+        public string Description { get; set; }
+        public decimal ProviderPrice { get; set; }
+        public decimal TradingPrice { get; set; }
+        public long Amount { get; set; }
+    }
+}
